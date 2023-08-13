d13254fbb9dc4cc1b51dc2b13aefc034cf81380d
maocastro13 <maocastro13@gmail.com>
2023-08-13 05:18:21 -0500

    Player Animations 65%

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float speed;
-    public Transform targetCam;
     float horizontalInput;
     float verticalInput;
 
@@ -24,23 +23,22 @@ public class Player : MonoBehaviour
     bool isMoving;
     Transform targetPosition;
 
-
+    public Transform targetCam;
     public float mouseSensibility = 2.0f;
+    public PlayerAnimationController animationController;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animationController = GetComponent<PlayerAnimationController>();
 
         isFirstPerson = false;
         isMoving = false;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (!blokedMovement)
         {
             if (isFirstPerson)
@@ -51,7 +49,6 @@ public class Player : MonoBehaviour
             {
                 ThirdPersonMovement();
             }
-
         }
         else
         {
@@ -63,6 +60,10 @@ public class Player : MonoBehaviour
             isFirstPerson = !isFirstPerson;
         }
 
+        if (Input.GetKeyDown(KeyCode.M)) // Detectar la pulsación de la tecla 'M'
+        {
+            animationController.SetEasterEgg(); // Llamar al método SetEasterEgg
+        }
     }
 
     public void SetThirdPerson()
@@ -78,7 +79,8 @@ public class Player : MonoBehaviour
 
     private void ThirdPersonMovement()
     {
-        if (!canMove) return;
+        if (!canMove)
+            return;
 
         GetInput();
         Vector3 movement = new Vector3(0, 0.0f, verticalInput);
@@ -86,19 +88,37 @@ public class Player : MonoBehaviour
         rb.AddForce(globalMovement * speed);
         float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
 
-       
+        if (verticalInput != 0 || horizontalInput != 0) // Si hay movimiento
+        {
+            animationController.SetWalk(); // Configurar la animación de caminar
+        }
+        else
+        {
+            animationController.SetIdle(); // Configurar la animación de Idle
+        }
+
         transform.Rotate(Vector3.up, rotation);
     }
 
     private void FirstPersonMovement()
     {
-        if (!canMove) return;
-        
+        if (!canMove)
+            return;
+
         GetInput();
         Vector3 movimiento = new Vector3(horizontalInput, 0.0f, verticalInput);
         Vector3 movimientoGlobal = transform.TransformDirection(movimiento);
         rb.AddForce(movimientoGlobal * speed);
         transform.rotation = Quaternion.Euler(0, targetCam.rotation.eulerAngles.y, 0);
+
+        if (verticalInput != 0 || horizontalInput != 0) // Si hay movimiento
+        {
+            animationController.SetWalk(); // Configurar la animación de caminar
+        }
+        else
+        {
+            animationController.SetIdle(); // Configurar la animación de Idle
+        }
     }
 
     private void GetInput()
