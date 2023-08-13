d1d3589cc3b4a0bf5c770aa52968bd0e12f430b6
‘chrisgrm’ <chrisgrm@hotmail.com>
2023-08-13 02:01:57 -0500

    camaras topDown configuradas, inicio de mecanica de agarrar texto, npcs 90%

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,12 +9,17 @@ public class UIManager : MonoBehaviour
 {
     public TMP_Text dialogueText;
     public GameObject dialogePanel;
-    
+    public GameObject inputField;
+
+ 
+
     // Start is called before the first frame update
     void Start()
     {
         dialogePanel.SetActive(false);
-    }
+        inputField.SetActive(false);
+        
+}
 
     // Update is called once per frame
     void Update()
@@ -37,5 +42,33 @@ public class UIManager : MonoBehaviour
     {
         dialogePanel.SetActive(false);
 
+    }
+
+    internal void ActivateInputField()
+    {
+        StartCoroutine(ActivateInputCorroutine());
+    }
+
+    IEnumerator ActivateInputCorroutine()
+    {
+        yield return new WaitForSeconds(2);
+        inputField.SetActive(true);
+        print("holaa");
+        TMP_InputField input = inputField.GetComponentInChildren<TMP_InputField>();
+        input.Select();
+        input.ActivateInputField();
+    }
+    public void DeactivateInput()
+    {
+        inputField.SetActive(false);
+        TMP_InputField input = inputField.GetComponentInChildren<TMP_InputField>();     
+        input.text = "";
+
+    }
+
+    public string GetInputText()
+    {
+        TMP_InputField input= inputField.GetComponentInChildren<TMP_InputField>();
+        return input.text;
     }
 }
